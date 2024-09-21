540bfb07beeb76326204769a8cc8f726d064453f
akshaypx <akshaysrivastava46@gmail.com>
2024-09-21 17:50:25 +0530

    feat: db connection added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log("hello typescript");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(compression());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log(`Server listening on http://localhost:${process.env.PORT}/`);
+});
+
+//database connection
+mongoose.connect(process.env.MONGO_URI);
+const connection = mongoose.connection;
+connection.on("error", (err) => {
+  console.log(err);
+});
+connection.once("connected", () => {
+  console.log("Database Connected");
+});
